4b8837f8155bd3d60ee82f40f9ab8c2de9dfc6eb
YJ <lyjoo7413@naver.com>
2021-08-26 14:15:24 +0900

    Add. step6 comment

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,15 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
- ], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+ ], function (Controller, MessageToast) {
 	"use strict";
+	// Controller 를 상속하여 Retrun 해준다.
+	// 첫 번째 인자는 Controller의 fullname이다.
+	// 2번째인자는 object이다. Event Handler정보 javascript의 형식으로 넣어주면 된다. 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-		onShowHello : function () {
-			// show a native JavaScript alert
-			alert("Hello World");
-		}
+	   onShowHello : function () {
+		   // show a native javascript alert
+		  MessageToast.show("Hello World");
+	   }
 	});
  });
